ea4406844e957d6f5d070caf7a19649d30626931
TippyTappyFingers <44480895+TippyTappyFingers@users.noreply.github.com>
2018-11-21 13:08:55 -0800

    Updated on 11/21/18
    asdfadfasdf

--- wizard-C.cpp
+++ wizard-C.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello World!" << endl;
-  return 0;
-}
-
-std::ifstream App("App.py", std::ios::in);
-if (App.is_open()) {
-  std::string line;
-  while (std::getline(App, line)) {
-    std::cout << line << '\n';
+  std::ifstream App("App.py", std::ios::in);
+  if (App.is_open()) {
+    std::string line;
+    while (std::getline(App, line)) {
+      std::cout << line << '\n';
+    }
+    App.close();
   }
-  App.close();
-}
-else {
-  std::cerr << "Unable to open file\n";
+  else {
+    std::cerr << "Unable to open file\n";
+  }
 }
